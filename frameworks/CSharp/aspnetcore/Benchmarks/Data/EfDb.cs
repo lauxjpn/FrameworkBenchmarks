c3fa2ef65a4061851f5de3f272a3160b787758f1
Laurents Meyer <laucomm@gmail.com>
2021-05-17 01:31:24 +0200

    CPU variation: Improve LoadMultipleUpdatesRows performance.

--- frameworks/CSharp/aspnetcore/Benchmarks/Data/EfDb.cs
+++ frameworks/CSharp/aspnetcore/Benchmarks/Data/EfDb.cs
@@ -54,23 +54,18 @@ namespace Benchmarks.Data
         public async Task<World[]> LoadMultipleUpdatesRows(int count)
         {
             var results = new World[count];
-            int currentValue, newValue;
-
-            var ids = Enumerable.Range(1, 10000).Select(x => _random.Next(1, 10001)).Distinct().Take(count).ToArray();
-
+            
             for (var i = 0; i < count; i++)
             {
-                results[i] = await _firstWorldTrackedQuery(_dbContext, ids[i]);
-
-                currentValue = results[i].RandomNumber;
-
+                int id;
                 do
                 {
-                    newValue = _random.Next(1, 10001);
-                }
-                while (newValue == currentValue);
+                    id = _random.Next(1, 10001);
+                } while (results.Take(i).Any(r => r.Id == id));
+                
+                results[i] = await _firstWorldTrackedQuery(_dbContext, id);
 
-                results[i].RandomNumber = newValue;
+                results[i].RandomNumber = _random.Next(1, 10001);
 
                 _dbContext.Entry(results[i]).State = EntityState.Modified;
             }
